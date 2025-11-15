d663f6cd8fac69534a33c5cd3a611d0230284120
idbp <abdrb22@yahoo.com>
2025-11-15 22:24:57 +0300

    update speed

--- js/links.js
+++ js/links.js
@@ -1,10 +1,7 @@
 var website = ""; var link = "";
 function funclinks(){
 link += "<li><a href='" + website + "index.html'>الصفحة الرئيسية</a></li>";
-
 link += "<li><a href='" + website + "reqactive.html'>طلب رمز تفعيل</a></li>";
-
 link += "<li><a href='" + website + "contact.html'>تواصل</a></li>";
-
 document.getElementById("links").innerHTML=link;
 }
