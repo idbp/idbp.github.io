97d85c7ee8f305be64c78c2b43a962f36474fac3
idbp <abdrb22@yahoo.com>
2026-07-04 09:58:27 +0300

    update index

--- js/links.js
+++ js/links.js
@@ -1,7 +1,7 @@
 var website = ""; var link = "";
 function funclinks(){
 link += "<li><a href='" + website + "index.html'>الصفحة الرئيسية</a></li>";
-link += "<li><a href='" + website + "reqactive.html'>طلب رمز تفعيل</a></li>";
+link += "<li><a href='" + website + "#download'>تجريبي</a></li>";
 link += "<li><a href='" + website + "contact.html'>تواصل</a></li>";
 document.getElementById("links").innerHTML=link;
 }
